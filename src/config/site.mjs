ab70aae4b16e2d4ff93997b24e4280d39b3870da
Samy Rahmani <samy.osmium@gmail.com>
2026-02-08 21:15:19 -0500

    feat: init

---
+++ src/config/site.mjs
@@ -0,0 +1,10 @@
+export const SITE = {
+	title: "Protech | Club de Programmation et de Technologie",
+	description:
+		"Club de Programmation et de Technologie du Collège de Maisonneuve",
+	defaultLanguage: "fr",
+	url: "https://protech.sh", // Update this with your actual site URL
+	author: "Astro Base",
+	ogImage: "/og-image.png", // Add this image to your public folder
+	web3formsAccessKey: "182a7873-78e9-4f0b-9b58-01aa19294378",
+};
